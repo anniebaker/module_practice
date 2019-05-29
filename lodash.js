08003901fee05c5eb7c3396283713bdd38f750c7
anniebaker <bakeranneabb@gmail.com>
2019-05-28 20:01:20 -0500

    get down lodash

--- lodash.js
+++ lodash.js
@@ -1,14 +1,54 @@
-// import _ from "lodash";
+"use strict";
 const _ = require("lodash");
+const assert = require("assert");
 
 let array = [1, 3, 5, 6, 8];
 const chunkArr = _.chunk(array, 2);
 console.log(chunkArr);
 
-console.log(_.reverse(array));
+const reverseArr = _.reverse(array);
+console.log(reverseArr);
 
-console.log(_.without(array, 5));
+const withoutArr = _.without(array, 5);
+console.log(withoutArr);
 
-console.log(_.initial(array));
+const logInitial = _.initial(array);
+console.log(logInitial);
 
-console.log(_.flatten(chunkArr));
+const flatArr = _.flatten(chunkArr);
+console.log(flatArr);
+
+// TESTS:
+
+if (typeof describe === "function") {
+  describe("chunk test", () => {
+    it("should chunk the dataArr", () => {
+      assert.deepEqual(chunkArr, [[1, 3], [5, 6], [8]]);
+    });
+  });
+  describe("reverse test", () => {
+    it("should reverse the dataArr", () => {
+      assert.deepEqual(reversed, [8, 6, 5, 3, 1]);
+    });
+  });
+
+  describe("without 5 test", () => {
+    it("should remove 5 from the dataArr", () => {
+      assert.deepEqual(withoutArr, [8, 6, 3, 1]);
+    });
+  });
+
+  describe("initial test", () => {
+    it("should drop the first value in the dataArr", () => {
+      assert.deepEqual(logInitial, [8, 6, 5, 3]);
+    });
+  });
+
+  describe("join test", () => {
+    it("should flatten the dataArr into a single array", () => {
+      assert.deepEqual(flatArr, [1, 3, 5, 6, 8]);
+    });
+  });
+} else {
+  console.log("not working");
+}
